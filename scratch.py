9e8ece50df2c88f7215371831497a94388c02574
SamCalliskan <erdincscaliskan@gmail.com>
2025-11-13 23:26:09 +0300

    Implement battery search logic in scratch.py

--- scratch.py
+++ scratch.py
@@ -1,2 +1,27 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+
+import random
+import time
+
+yerler = ["Laboratuvar", "atölye", "depo", "çatı", "garaj"]
+
+enerji = 100
+buldu_mu = False
+
+print("Pyro bataryasını aramaya başladı...\n")
+
+for yer in yerler:
+    print(f" {Yer} kontrol ediliyor...")
+    time.sleep(1)
+    enerji -= 20
+
+    if random.choice([True, False]):
+        print(f" Pyro bataryasını {yer} içinde buldu!")
+        buldu_mu = True
+        break
+    else:
+        print(f" {yer} içinde batarya yok. Enerji: {enerji}%\n")
+
+if buldu_mu:
+    print("\n Görev tamamlandı! Pyro yeniden şari oluyor...")
+else:
+    print("\n Pyro bataryayı bulamadı... Enerjisi bitti. Yeniden dene!")
